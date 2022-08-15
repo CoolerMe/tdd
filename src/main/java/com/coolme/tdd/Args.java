eb9ac22ef945c727d2a7382bd642e55d7d53e886
coolme <cooldeveloper@foxmail.com>
2022-08-15 08:14:04 +0800

    Clean code

--- src/main/java/com/coolme/tdd/Args.java
+++ src/main/java/com/coolme/tdd/Args.java
@@ -12,30 +12,44 @@ import java.util.Map;
  */
 public class Args {
 
-  public static Map<Class<?>, OptionParser<?>> PARSER = Map.of(
-      int.class, OptionParsers.unary(0, Integer::parseInt),
-      String.class, OptionParsers.unary("", String::valueOf),
-      boolean.class, OptionParsers.bool(),
-      Integer[].class, OptionParsers.list(Integer[]::new, Integer::parseInt),
-      String[].class, OptionParsers.list(String[]::new, String::valueOf)
-  );
+    public static Map<Class<?>, OptionParser<?>> PARSER = Map.of(
+        int.class, OptionParsers.unary(0, Integer::parseInt),
+        String.class, OptionParsers.unary("", String::valueOf),
+        boolean.class, OptionParsers.bool(),
+        Integer[].class, OptionParsers.list(Integer[]::new, Integer::parseInt),
+        String[].class, OptionParsers.list(String[]::new, String::valueOf)
+    );
 
-  public static <T> T parse(Class<T> clazz, String... args) {
 
-    try {
-      Constructor<?>[] constructors = clazz.getDeclaredConstructors();
-      List<String> arguments = Arrays.asList(args);
-      Parameter[] parameters = constructors[0].getParameters();
-
-      Object[] values = Arrays.stream(parameters)
-          .map(parameter -> OptionParsers.parse(arguments, parameter))
-          .toArray();
-      return (T) constructors[0].newInstance(values);
-    } catch (IllegalOptionException e) {
-      throw e;
-    } catch (Exception e) {
-      throw new RuntimeException(e);
+    public static <T> T parse(Class<T> clazz, String... args) {
+        return parse(PARSER, args, clazz);
     }
-  }
 
+    private static <T> T parse(Map<Class<?>, OptionParser<?>> parser, String[] args,
+        Class<T> option) {
+        try {
+            Constructor<?>[] constructors = option.getDeclaredConstructors();
+            List<String> arguments = Arrays.asList(args);
+            Parameter[] parameters = constructors[0].getParameters();
+
+            Object[] values = Arrays.stream(parameters)
+                .map(parameter -> parse(arguments, parameter, parser))
+                .toArray();
+            return (T) constructors[0].newInstance(values);
+        } catch (IllegalOptionException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Object parse(List<String> arguments, Parameter parameter,
+        Map<Class<?>, OptionParser<?>> parser) {
+        if (!parameter.isAnnotationPresent(Option.class)) {
+            throw new IllegalOptionException(parameter.getName());
+        }
+
+        return parser.get(parameter.getType())
+            .parse(arguments, parameter.getAnnotation(Option.class));
+    }
 }
--- src/main/java/com/coolme/tdd/OptionParsers.java
+++ src/main/java/com/coolme/tdd/OptionParsers.java
@@ -13,75 +13,70 @@ import java.util.stream.IntStream;
 
 class OptionParsers<T> {
 
-  public static OptionParser<Boolean> bool() {
-    return (arguments, option) -> values(arguments, option, 0).isPresent();
-  }
-
-  public static <T> OptionParser<T> unary(T defaultValue, Function<String, T> parser) {
-    return (arguments, option) -> values(arguments, option, 1).map(
-        strings -> parseValue(option, strings.get(0), parser)).orElse(defaultValue);
-  }
-
-  public static <T> OptionParser<T[]> list(IntFunction<T[]> generator, Function<String, T> parser) {
-    return (arguments, option)
-        -> values(arguments, option)
-        .map(it ->
-            it.stream()
-                .map(value -> parseValue(option, value, parser))
-                .toArray(generator))
-        .orElse(generator.apply(0));
-
-  }
-
-  static Object parse(List<String> arguments, Parameter parameter) {
-    if (!parameter.isAnnotationPresent(Option.class)) {
-      throw new IllegalOptionException(parameter.getName());
-    }
-    return Args.PARSER.get(parameter.getType())
-        .parse(arguments, parameter.getAnnotation(Option.class));
-  }
-
-
-  private static Optional<List<String>> values(List<String> arguments, Option option) {
-    int index = arguments.indexOf("-" + option.value());
-    return Optional.ofNullable(index == -1 ? null : values(arguments, index));
-  }
-
-  private static Optional<List<String>> values(List<String> arguments, Option option,
-      int expectedSize) {
-    return values(arguments, option)
-        .map(strings -> checkSize(option, expectedSize, strings));
-  }
-
-  private static List<String> checkSize(Option option, int expectedSize, List<String> values) {
-    if (values.size() < expectedSize) {
-      throw new InsufficientArgumentsException(option.value());
+    public static OptionParser<Boolean> bool() {
+        return (arguments, option) -> values(arguments, option, 0).isPresent();
     }
 
-    if (values.size() > expectedSize) {
-      throw new TooManyArgumentsException(option.value());
+    public static <T> OptionParser<T> unary(T defaultValue, Function<String, T> parser) {
+        return (arguments, option) -> values(arguments, option, 1).map(
+            strings -> parseValue(option, strings.get(0), parser)).orElse(defaultValue);
     }
 
-    return values;
+    public static <T> OptionParser<T[]> list(IntFunction<T[]> generator,
+        Function<String, T> parser) {
+        return (arguments, option)
+            -> values(arguments, option)
+            .map(it ->
+                it.stream()
+                    .map(value -> parseValue(option, value, parser))
+                    .toArray(generator))
+            .orElse(generator.apply(0));
 
-  }
-
-
-  private static <T> T parseValue(Option option, String value, Function<String, T> parser) {
-
-    try {
-      return parser.apply(value);
-    } catch (Exception e) {
-      throw new IllegalValueException(option.value(), value);
     }
-  }
 
-  private static List<String> values(List<String> arguments, int index) {
-    int followedIndex = IntStream.range(index + 1, arguments.size())
-        .filter(it -> arguments.get(it).matches("^-[a-zA-z-]+$"))
-        .findFirst().orElse(arguments.size());
 
-    return arguments.subList(index + 1, followedIndex);
-  }
+
+
+    private static Optional<List<String>> values(List<String> arguments, Option option) {
+        int index = arguments.indexOf("-" + option.value());
+        return Optional.ofNullable(index == -1 ? null : values(arguments, index));
+    }
+
+    private static Optional<List<String>> values(List<String> arguments, Option option,
+        int expectedSize) {
+        return values(arguments, option)
+            .map(strings -> checkSize(option, expectedSize, strings));
+    }
+
+    private static List<String> checkSize(Option option, int expectedSize, List<String> values) {
+        if (values.size() < expectedSize) {
+            throw new InsufficientArgumentsException(option.value());
+        }
+
+        if (values.size() > expectedSize) {
+            throw new TooManyArgumentsException(option.value());
+        }
+
+        return values;
+
+    }
+
+
+    private static <T> T parseValue(Option option, String value, Function<String, T> parser) {
+
+        try {
+            return parser.apply(value);
+        } catch (Exception e) {
+            throw new IllegalValueException(option.value(), value);
+        }
+    }
+
+    private static List<String> values(List<String> arguments, int index) {
+        int followedIndex = IntStream.range(index + 1, arguments.size())
+            .filter(it -> arguments.get(it).matches("^-[a-zA-z-]+$"))
+            .findFirst().orElse(arguments.size());
+
+        return arguments.subList(index + 1, followedIndex);
+    }
 
 }
